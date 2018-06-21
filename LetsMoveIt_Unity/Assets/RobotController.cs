691fcb48f10d1430addb42f268647ce8eba9de89
Nevin <nevin-wouters@hotmail.com>
2018-06-21 18:05:40 +0200

    Warehouse Layout

---
+++ LetsMoveIt_Unity/Assets/RobotController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+
+	void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
