f7cb30272ef95ab64eda9cf723dfc96cae42d5ff
Nevin <nevin-wouters@hotmail.com>
2018-07-12 07:56:37 +0200

    last version

---
+++ LetsMoveIt_Unity/Assets/ZoomFunction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomFunction : MonoBehaviour {
+
+    Camera cam;
+    public float minZoom = 10f;
+    public float maxZoom = 50f;
+
+    public float zoomSpeed = 5f;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update () {
+    
+        if(Input.GetKey(KeyCode.Plus) || Input.GetKey(KeyCode.KeypadPlus) || Input.GetKey(KeyCode.W))
+        {
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - zoomSpeed * Time.deltaTime, minZoom, maxZoom);
+        }
+
+        if(Input.GetKey(KeyCode.Minus) || Input.GetKey(KeyCode.KeypadMinus) || Input.GetKey(KeyCode.S))
+        {
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize + zoomSpeed * Time.deltaTime, minZoom, maxZoom);
+        }
+	}
+}
